24b22147bc89de43bcafdece2e4cda09ae4b931b
Sangbaek Lee <nupamore@gmail.com>
2020-05-01 01:43:54 +0900

    fix: err

--- router.js
+++ router.js
@@ -6,11 +6,22 @@ const QUERY = require('./query')
 const pool = mysql.createPool(CONFIG.db)
 
 router.get('/struct/:id', async (req, res) => {
-    const [rows] = await pool.query(QUERY.GET_STRUCT, [req.params.id])
-    if (rows.length) res.json(rows[0])
-    else res.status(404).json({
-        message: "해당 아이디를 가진 데이터를 찾을 수 않음"
-    })
+    try {
+        const [rows] = await pool.query(QUERY.GET_STRUCT, [req.params.id])
+        if (rows.length) res.json({
+            code: 200,
+            data: rows[0],
+        })
+        else res.status(400).json({
+            code: 404,
+            message: "해당 아이디를 가진 데이터를 찾을 수 않음"
+        })
+    } catch (e) {
+        res.status(400).json({
+            code: e.errno,
+            message: e.message,
+        })
+    }
 })
 
 router.post('/struct/:id', async (req, res) => {
@@ -21,41 +32,61 @@ router.post('/struct/:id', async (req, res) => {
             req.body.content,
         ])
         res.json({
-            message: "생성 성공"
+            code: 200,
+            message: "생성 성공",
         })
     } catch (e) {
-        res.status(409).json({
-            message: "해당 아이디를 가진 데이터가 이미 존재함"
+        res.status(400).json({
+            code: e.errno,
+            message: e.message,
         })
     }
 })
 
 router.put('/struct/:id', async (req, res) => {
     try {
-        const [rows] = await pool.query(QUERY.UPDATE_STRUCT, [
+        const [{changedRows}] = await pool.query(QUERY.UPDATE_STRUCT, [
             req.body.type,
             req.body.content,
             req.params.id,
         ])
-        res.json({
-            message: "수정 성공"
-        })
+        if (changedRows) {
+            res.json({
+                code: 200,
+                message: "수정 성공",
+            })
+        } else {
+            res.status(400).json({
+                code: 404,
+                message: "해당 아이디를 가진 데이터를 찾을 수 않음"
+            })
+        }
     } catch (e) {
-        res.status(404).json({
-            message: "해당 아이디를 가진 데이터를 찾을 수 않음"
+        res.status(400).json({
+            code: e.errno,
+            message: e.message,
         })
     }
 })
 
 router.delete('/struct/:id', async (req, res) => {
     try {
-        const [rows] = await pool.query(QUERY.DELETE_STRUCT, [req.params.id])
-        res.json({
-            message: "삭제 성공"
-        })
+        const [{changedRows}] = await pool.query(QUERY.DELETE_STRUCT, [req.params.id,])
+        if (changedRows) {
+            res.json({
+                code: 200,
+                message: "수정 성공",
+            })
+        } else {
+            res.status(400).json({
+                code: 404,
+                message: "해당 아이디를 가진 데이터를 찾을 수 않음"
+            })
+        }
     } catch (e) {
-        res.status(404).json({
-            message: "해당 아이디를 가진 데이터를 찾을 수 않음"
+        res.status(400).json({
+            code: e.errno,
+            message: e.message,
         })
     }
 })
