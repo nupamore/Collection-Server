63a0474ad77a167932d07accf05eaabf4fe69772
Sangbaek Lee <nupamore@gmail.com>
2020-06-02 20:56:18 +0900

    feat: list api

--- api/haveCharacters.js
+++ api/haveCharacters.js
@@ -1,6 +1,21 @@
 const router = require('express').Router()
 const db = require('../db/driver')
 
+router.get('/haveCharacters', async (req, res) => {
+    try {
+        const result = await db.models.haveCharacters.findAll()
+        res.status(200).json({
+            code: 200,
+            data: result,
+        })
+    } catch (e) {
+        res.status(400).json({
+            code: 400,
+            message: e.toString(),
+        })
+    }
+})
+
 router.get('/haveCharacters/:id', async (req, res) => {
     try {
         const result = await db.models.haveCharacters.findOne({
--- api/members.js
+++ api/members.js
@@ -1,6 +1,21 @@
 const router = require('express').Router()
 const db = require('../db/driver')
 
+router.get('/members', async (req, res) => {
+    try {
+        const result = await db.models.members.findAll()
+        res.status(200).json({
+            code: 200,
+            data: result,
+        })
+    } catch (e) {
+        res.status(400).json({
+            code: 400,
+            message: e.toString(),
+        })
+    }
+})
+
 router.get('/members/:id', async (req, res) => {
     try {
         const result = await db.models.members.findOne({
