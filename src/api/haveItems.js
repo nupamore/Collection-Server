3c30d42fc603e202c789032e299a6d4ba9c835eb
hoon <cjsstk805@xlgames.com>
2020-07-10 15:24:53 +0900

    feat. haveItems 데이터 읽기, 생성 추가

--- src/api/haveItems.js
+++ src/api/haveItems.js
@@ -1,12 +1,37 @@
 const router = require('express').Router()
 const db = require('../db/driver')
 
-router.get('/haveItems', async (req, res) => {
+// 특정 유저가 가진 아이템들을 가져오는 api
+router.get('/members/:memberId/haveItems', async (req, res) => {
     try {
-        const result = await db.models.haveItems.findAll()
+        const result = await db.models.haveItems.findAll({
+            where: { memberId: req.params.memberId },
+        })
         res.status(200).json({
             code: 200,
             data: result,
+        })
+    } catch (e) {
+        res.status(400).json({
+            code: 400,
+            message: e.toString(),
+        })
+    }
+})
+
+// 특정 유저에게 새로운 아이템들을 추가하는 api
+router.post('/members/:memberId/haveItems', async (req, res) => {
+    const { memberId } = req.params
+    const { items } = req.body
+    items.forEach(item => {
+        item.memberId = memberId
+    })
+
+    try {
+        await db.models.haveItems.bulkCreate(items)
+        res.status(200).json({
+            code: 200,
+            data: '생성 성공',
         })
     } catch (e) {
         res.status(400).json({
